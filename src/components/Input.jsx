5b9d6a3a6ab56022ade75a49ffd9d639bee47e42
itsamanverma <amanvermame786@gmail.com>
2019-03-03 16:26:12 +0530

    Login pages of FundooNotes in Reactjs uing the concept of @material-ui/core ,Textfield,ES6 features

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,2 +1,34 @@
 import React, { Component } from "react";
 import { TextFeild } from "@meterial-ui/core";
+
+
+export default class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: '',
+            color: this.props.color
+        }
+        this.sendDataToParent = this.sendDataToParent.bind(this);
+    }
+    sendDataToParent(event) {
+        this.setState({ data: event.targert.value })
+        this.props.onChange(event, event.target.value)
+    }
+
+    render() {
+        return (
+            <TextFeild
+                className='label'
+                name={this.props.name}
+                type={this.props.type}
+                placeholder={this.props.placeholder}
+                label={this.props.label}
+                onChange={this.sendDataToParent}
+                fullwidth
+                color={this.state.color}
+                required={this.props.required}
+            />
+        );
+    }
+} 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,87 @@
 import React, { Componenet } from 'react'
 import Input from "../components/Input"
 
-class Login extends Componenet
-{
-    
+class Login extends Componenet {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            isValid: '',
+            error: '',
+            isLoggedIn: false,
+        }
+        this.getDataFromInput = this.getDataFromInput.bind(this)
+        this.validate = this.validate.bind(this)
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    /**
+     * function to handle the input validation of the page
+     * @param {event} event
+     */
+    validate(event) {
+        var error = {
+            email ='',
+            password ='',
+        };
+        var invalid = true;
+
+        if (this.state.email.length < 1) {
+            error.email = 'Email invalid, Correct Email Required'
+            invalid = false;
+        }
+
+        if (this.state.password.length < 1) {
+            error.password = 'Password cannot be empty'
+            invalid = false;
+        }
+        this.setState({
+            error: error,
+        })
+        return invalid;
+    }
+
+    /**
+     * function to get data from the Object 
+     * @param {var} data
+     */
+    getDataFromInput(event, data) {
+        this.setState({
+            [event.target.name]: data
+        })
+    }
+
+    /**
+     * function to handle click of the button of login
+     * @param {event} event
+     */
+    handleClick() {
+        if (this.validate()) {
+            let data = {
+                email: this.state.email,
+                password: this.state.password,
+            }
+        }
+        userService.Login(data).then(response => {
+            console.log("asasa", response);
+            console.log(this.state);
+
+            if (response.status === 200) {
+                this.setState({
+                    isLoggedIn: true,
+                });
+            }
+            else if (response.status === 204) {
+                this.setState({
+                    error: {
+                        email: 'invalid credentails',
+                    }
+                })
+            }
+            else if (response.status === 211) {
+              
+            }
+        })
+    }
 }
