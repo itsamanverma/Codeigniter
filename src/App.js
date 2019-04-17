556ba5bd11bf3c0a7d69e2c0555c4297ea4e6322
itsamanverma <amanvermame786@gmail.com>
2019-04-17 19:34:15 +0530

    change in route file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-// import Login from "../pages/Login";
 import { BrowserRouter as Router,Route } from "react-router-dom";
 //import {Buttons} from "@material-ui/core/Button"
-//import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import "./App.css"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
